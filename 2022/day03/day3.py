89b9b21a9826ff8374c73e5b3021344a3efcecd0
Markus Thill <8896197+MarkusThill@users.noreply.github.com>
2023-01-13 23:09:59 +0100

    Update day3.py

--- 2022/day03/day3.py
+++ 2022/day03/day3.py
@@ -1,9 +1,3 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def day3_1():
     # Using readlines()
     file1 = open('input3_1.txt', 'r')
