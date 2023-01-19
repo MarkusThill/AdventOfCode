5a9404b937733008b48fbe313b1d7a3665bcf1f2
Markus Thill <8896197+MarkusThill@users.noreply.github.com>
2023-01-19 06:46:07 +0100

    Update day1.py

--- 2022/day01/day1.py
+++ 2022/day01/day1.py
@@ -1,14 +1,4 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def day1_1(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-    # Using readlines()
     file1 = open('input1_1.txt', 'r')
     lines = file1.readlines()
 
@@ -16,7 +6,6 @@ def day1_1(name):
     calories = 0
     for l in lines:
         l = l.strip()
-        #print(l)
         if len(l) == 0:
             #print("calories:", calories)
             cals_list.append(calories)
@@ -27,10 +16,6 @@ def day1_1(name):
     print("Solution day 1:", max(cals_list))
 
 def day1_2(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-    # Using readlines()
     file1 = open('input1_1.txt', 'r')
     lines = file1.readlines()
 
@@ -38,9 +23,7 @@ def day1_2(name):
     calories = 0
     for l in lines:
         l = l.strip()
-        #print(l)
         if len(l) == 0:
-            #print("calories:", calories)
             cals_list.append(calories)
             calories = 0
             continue
@@ -48,9 +31,7 @@ def day1_2(name):
     cals_list.append(calories)
     print("Solution day2:", sum(sorted(cals_list)[-3:]) )
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    day1_1('PyCharm')
-    day1_2('PyCharm')
+    day1_1()
+    day1_2()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
