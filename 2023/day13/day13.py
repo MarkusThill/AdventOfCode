7047b81a35b347330c7ac528e8c3e1e5a854a7a0
Markus Thill <8896197+MarkusThill@users.noreply.github.com>
2024-01-02 02:04:58 +0100

    day 13: slightly better solution

--- 2023/day13/day13.py
+++ 2023/day13/day13.py
@@ -1,6 +1,5 @@
 def find_mirror(puzzle, ignore=-1):
     N = len(puzzle)
-    ans = 0
     for x in range(0, N - 1):
         if x == ignore - 1:
             continue
@@ -12,7 +11,28 @@ def find_mirror(puzzle, ignore=-1):
         if i < 0 or j >= N:
             return x + 1
 
-    return ans
+    return 0
+
+
+def find_mirror(puzzle, ignore=-1, fix_smudge=False):
+    N = len(puzzle)
+    for x in range(0, N - 1):
+        if x == ignore - 1:
+            continue
+        i, j = x, x + 1
+        fs = fix_smudge
+        while i >= 0 and j < N:
+            d = sum(c1 != c2 for c1, c2 in zip(puzzle[i], puzzle[j]))
+            if not fs and d > 0 or d > 1:
+                break
+            if d == 1 and fs:
+                fs = False
+
+            i, j = i - 1, j + 1
+        if i < 0 or j >= N:
+            return x + 1
+
+    return 0
 
 
 def day13():
@@ -31,27 +51,41 @@ def day13():
 
     print(f"Solution Day 12.1: {100 * sum(rows) + sum(cols)}")
 
+    # new_rows, new_cols = [], []
+    # for p, r, c in zip(puzzles, rows, cols):
+    #     for i in range(len(p)):
+    #         for j in range(len(p[0])):
+    #             pp = list(p[i])
+    #             pp[j] = "#" if pp[j] == "." else "."
+    #             p[i] = "".join(pp)
+    #             rr = find_mirror(p, ignore=r if r != 0 else -1)
+    #             cc = find_mirror(
+    #                 ["".join(column) for column in zip(*p)],
+    #                 ignore=c if c != 0 else -1,
+    #             )
+    #             pp[j] = "#" if pp[j] == "." else "."
+    #             p[i] = "".join(pp)
+
+    #             if rr != 0 or cc != 0:
+    #                 new_rows.append(rr)
+    #                 new_cols.append(cc)
+    #                 break
+    #         if rr != 0 or cc != 0:
+    #             break
+
+    # print(f"Solution Day 12.2: {100 * sum(new_rows) + sum(new_cols)}")
+
     new_rows, new_cols = [], []
     for p, r, c in zip(puzzles, rows, cols):
-        for i in range(len(p)):
-            for j in range(len(p[0])):
-                pp = list(p[i])
-                pp[j] = "#" if pp[j] == "." else "."
-                p[i] = "".join(pp)
-                rr = find_mirror(p, ignore=r if r != 0 else -1)
-                cc = find_mirror(
-                    ["".join(column) for column in zip(*p)],
-                    ignore=c if c != 0 else -1,
-                )
-                pp[j] = "#" if pp[j] == "." else "."
-                p[i] = "".join(pp)
-
-                if rr != 0 or cc != 0:
-                    new_rows.append(rr)
-                    new_cols.append(cc)
-                    break
-            if rr != 0 or cc != 0:
-                break
+        rr = find_mirror(p, ignore=r if r != 0 else -1, fix_smudge=True)
+        cc = find_mirror(
+            ["".join(column) for column in zip(*p)],
+            ignore=c if c != 0 else -1,
+            fix_smudge=True,
+        )
+        if rr != 0 or cc != 0:
+            new_rows.append(rr)
+            new_cols.append(cc)
 
     print(f"Solution Day 12.2: {100 * sum(new_rows) + sum(new_cols)}")
 
