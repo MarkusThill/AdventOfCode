ebe061c0c5f539d147b3ad4c0b7d86e3a3381aad
m.thill <8896197+MarkusThill@users.noreply.github.com>
2024-01-06 08:01:38 +0100

    Day 18

---
+++ 2023/day18/day18.py
@@ -0,0 +1,120 @@
+import queue
+
+
+def shoelace(points):
+    """
+    https://en.wikipedia.org/wiki/Shoelace_formula
+    """
+    xx, yy = zip(*points)
+    return (
+        sum(
+            [
+                s1 - s2
+                for s1, s2 in zip(
+                    [x * y for x, y in zip(xx, list(yy[1:]) + [yy[0]])],
+                    [x * y for x, y in zip(list(xx[1:]) + [xx[0]], yy)],
+                )
+            ]
+        )
+    ) // 2
+
+
+def day18():
+    file1 = open("2023/day18/input.txt", "r")
+    lines = [l.strip() for l in file1.readlines()]
+    lines = [l.split(" ") for l in lines]
+
+    # Part 1:
+    points, perimeter = parse_part_1(lines)
+
+    # If we draw the points (x,y) into the center of each corresponding 1x1 block, we
+    # can see that we are missing some of the area. We can simply move the whole shape
+    # down & left by half a box to illustrate this. We will see that half (+1) of the
+    # 'trench' (perimeter) is missing. Hence, we add it here. This is somewhat related
+    # to Pick's theorem: https://en.wikipedia.org/wiki/Pick%27s_theorem
+    print(f"Solution Day 18.1: {abs(shoelace(points)) + perimeter // 2 + 1}")
+
+    # Part 2:
+    points, perimeter = parse_part_2(lines)
+    print(f"Solution Day 18.2: {abs(shoelace(points)) + perimeter // 2 + 1}")
+
+
+def parse_part_1(lines):
+    x, y = 0, 0
+    points, perimeter = [(x, y)], 0
+    for d, v, _ in lines:
+        v = int(v)
+        perimeter += v
+        if d == "R":
+            x, y = x + v, y
+        elif d == "L":
+            x, y = x - v, y
+        elif d == "U":
+            x, y = x, y + v
+        elif d == "D":
+            x, y = x, y - v
+        points.append((x, y))
+    return points, perimeter
+
+
+def parse_part_2(lines):
+    x, y = 0, 0
+    points, perimeter = [(x, y)], 0
+    for _, _, c in lines:
+        v, d = int(c[2:-2], 16), c[-2]
+        perimeter += v
+        if d == "0":
+            x, y = x + v, y
+        elif d == "2":
+            x, y = x - v, y
+        elif d == "3":
+            x, y = x, y + v
+        elif d == "1":
+            x, y = x, y - v
+        points.append((x, y))
+    return points, perimeter
+
+
+if __name__ == "__main__":
+    day18()
+
+
+def day18_part1_old():
+    file1 = open("2023/day18/input.txt", "r")
+    lines = [l.strip() for l in file1.readlines()]
+    lines = [l.split(" ") for l in lines]
+
+    x, y = 0, 0
+    outmost_left = x, y
+    points = {(x, y)}
+
+    for d, v, c in lines:
+        v = int(v)
+        for _ in range(v):
+            if d == "R":
+                x, y = x + 1, y
+            elif d == "L":
+                x, y = x - 1, y
+            elif d == "U":
+                x, y = x, y - 1
+            elif d == "D":
+                x, y = x, y + 1
+            points.add((x, y))
+
+            if x < outmost_left[0]:
+                outmost_left = x, y
+        print(x, y)
+
+    # Flood-fill inner side of 'lagoon'
+    x, y = outmost_left
+    q = [(x + 1, y + 1)]
+    while q:
+        p_x, p_y = q.pop()
+        for dx in range(-1, 2):
+            for dy in range(-1, 2):
+                pp = p_x + dx, p_y + dy
+                if pp not in points:
+                    q.append(pp)
+                    points.add(pp)
+
+    print(f"Solution Day 18.1: {len(points)}")
